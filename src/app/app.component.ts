2ce277ddd7453ef595e0805f4b9575d7e878f9a2
Daniel <daniele.munozh@uqvirtual.edu.co>
2025-03-18 15:04:38 -0500

    implementacion de lista de logos de aliados

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,15 @@ import { Component } from '@angular/core';
 import { TopbarComponent } from './topbar/topbar.component';
 import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { LogoListComponent } from "./logo-list/logo-list.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [TopbarComponent, RouterModule, WelcomeComponent] // Importa RouterModule aquí
+  imports: [TopbarComponent, RouterModule, WelcomeComponent, LogoListComponent] // Importa RouterModule aquí
+ // Importa RouterModule aquí
 })
 export class AppComponent {
   title = 'seguRed';
---
+++ src/app/logo-list/logo-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logo-list',
+  imports: [],
+  templateUrl: './logo-list.component.html',
+  styleUrl: './logo-list.component.css'
+})
+export class LogoListComponent {
+
+}
