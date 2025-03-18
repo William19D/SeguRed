9d07bb395275d1d4ff62680ba9c1d73e91c7c3b3
Daniel <daniele.munozh@uqvirtual.edu.co>
2025-03-18 17:43:38 -0500

    nuevo bloque

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,22 @@
-// filepath: d:\Segured\SeguRed\SeguRed\src\app\app.component.ts
 import { Component } from '@angular/core';
 import { TopbarComponent } from './topbar/topbar.component';
 import { RouterModule } from '@angular/router';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { LogoListComponent } from "./logo-list/logo-list.component";
+import { CaracteristicasComponent } from "./caracteristicas/caracteristicas.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [TopbarComponent, RouterModule, WelcomeComponent, LogoListComponent] // Importa RouterModule aquí
- // Importa RouterModule aquí
+  imports: [
+    TopbarComponent,
+    RouterModule,
+    WelcomeComponent,
+    LogoListComponent,
+    CaracteristicasComponent // Asegúrate de que este componente esté correctamente importado
+  ]
 })
 export class AppComponent {
   title = 'seguRed';
