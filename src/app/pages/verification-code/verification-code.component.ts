cbe9b7778268e2e1833fd07997260eda9941f388
Daniel <daniele.munozh@uqvirtual.edu.co>
2025-03-28 16:38:18 -0500

    implementacion de autentificacion

--- src/app/pages/verification-code/verification-code.component.ts
+++ src/app/pages/verification-code/verification-code.component.ts
@@ -1,6 +1,7 @@
 import { Component, ElementRef, ViewChild, ViewChildren, QueryList } from '@angular/core';
 import { FooterComponent } from '../../shared/components/footer/footer.component';
 import { TopbarComponent } from '../../shared/components/topbar/topbar.component';
+import { AuthService } from '../../core/services/authentication.service';
 
 @Component({
   selector: 'app-verificacion-correo',
@@ -9,27 +10,46 @@ import { TopbarComponent } from '../../shared/components/topbar/topbar.component
   styleUrls: ['./verification-code.component.css'],
   imports: [
     FooterComponent,
-    TopbarComponent],
+    TopbarComponent
+  ],
 })
 export class VerificationCodeComponent {
   
   @ViewChildren('code1, code2, code3, code4, code5, code6') inputs!: QueryList<ElementRef>;
 
+  constructor(private authService: AuthService) {}
+
   moverFoco(event: any, index: number) {
     const inputValue = event.target.value;
+    const inputsArray = this.inputs.toArray();
+    
     if (inputValue && index < 6) {
-      this.inputs.get(index)?.nativeElement.focus();
+      inputsArray[index]?.nativeElement.focus();
+    }
+  }
+
+  borrarFoco(event: KeyboardEvent, index: number) {
+    if (event.key === 'Backspace' && index > 0) {
+      const inputsArray = this.inputs.toArray();
+      inputsArray[index - 1]?.nativeElement.focus();
     }
   }
 
   verificarCodigo() {
-    const codigoIngresado = this.inputs.map(input => input.nativeElement.value).join('');
-    console.log('Código ingresado:', codigoIngresado);
+    const inputsArray = this.inputs.toArray();
+    const codigoIngresado = inputsArray.map(input => input.nativeElement.value).join('');
 
     if (codigoIngresado.length === 6) {
-      alert('Código verificado correctamente');
+      this.authService.verificarCodigo(codigoIngresado).subscribe(
+        (response: { message: string }) => {
+          alert(response.message);
+        },
+        (error: { message: string }) => {
+          alert('Código incorrecto o expirado.');
+        }
+      );
     } else {
-      alert('Por favor, completa los 6 dígitos');
+      alert('Por favor, completa los 6 dígitos.');
     }
   }
 }
