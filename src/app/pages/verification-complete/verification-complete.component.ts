7d44f727b85947b4221faffb9f642d90a18b76da
Daniel <daniele.munozh@uqvirtual.edu.co>
2025-03-29 22:50:27 -0500

    creacion de verificacion correcta de codigo

---
+++ src/app/pages/verification-complete/verification-complete.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-verification-complete',
+  imports: [],
+  templateUrl: './verification-complete.component.html',
+  styleUrl: './verification-complete.component.css'
+})
+export class VerificationCompleteComponent {
+
+}
