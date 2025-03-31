599ce5d9ca3595cfa28d09d4dff2dc64089ddc9b
Luis Carlos Calderón Calvo <luisc.calderonc@uqvirtual.edu.co>
2025-03-31 11:53:57 -0500

    feat: Se crea una animación spin para la espera al componente de verificación de códgio

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -6,14 +6,14 @@ import { FooterComponent } from '../../shared/components/footer/footer.component
 import { Router } from '@angular/router';
 import { ApiService } from '../../core/services/api.service';
 import { AuthService } from '../../core/services/authentication.service';
-import { LocationService } from '../../core/services/location.service'; // Importar el servicio de ubicación
+import { LocationService } from '../../core/services/location.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css'],
-  imports: [TopbarComponent, FooterComponent, FormsModule],
+  imports: [TopbarComponent, FooterComponent, FormsModule, NgIf],
 })
 export class RegisterComponent {
   constructor(
@@ -22,6 +22,8 @@ export class RegisterComponent {
     private authService: AuthService,
     private locationService: LocationService
   ) {} // Inyectar Router en el constructor
+
+  isLoading: boolean=false; // Variable para controlar el estado de carga de animación
 
   user = {
     name: '',
@@ -62,7 +64,7 @@ export class RegisterComponent {
       direccion: this.user.address,
       telefono: this.user.phone,
       cargo: '',
-      estado: 'En espera',
+      estado: 'EN_ESPERA',
       correo: this.user.email,
       preferencias: '',
       contraseña: this.user.password,
@@ -83,19 +85,25 @@ export class RegisterComponent {
   }
 
   sendVerificationEmail(email: string) {
+    this.isLoading = true; // Activar la animación de carga
+    
     this.authService.sendVerificationCode(email).subscribe(
       (response: any) => {
-        // Especificar el tipo 'any' para response
         console.log('Correo de verificación enviado', response);
         console.log(
           `Redirigiendo a /verification-code/${encodeURIComponent(email)}`
         );
-        this.router.navigate([
-          `/verification-code/${encodeURIComponent(email)}`,
-        ]); // Redirigir a la página de verificación de código con el correo
+        
+        // Un timeout para simular la carga de la animación
+        setTimeout(() => {
+          this.isLoading = false;
+          this.router.navigate([
+            `/verification-code/${encodeURIComponent(email)}`,
+          ]);
+        }, 1000);
       },
       (error: any) => {
-        // Especificar el tipo 'any' para error
+        this.isLoading = false; // Set loading to false if there's an error
         console.log('Error al enviar el correo de verificación', error);
       }
     );
