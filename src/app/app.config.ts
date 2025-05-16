3e8245d1774ba2a97971f7f9689a9126d0683864
Daniel <daniele.munozh@uqvirtual.edu.co>
2025-05-16 14:16:51 -0500

    correcion ruta interceptor

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,7 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideHttpClient, withInterceptors, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthInterceptor } from '.app/core/interceptors/auth.interceptor';
+import { AuthInterceptor } from './core/interceptors/auth.interceptors';
 
 export const appConfig: ApplicationConfig = {
   providers: [
